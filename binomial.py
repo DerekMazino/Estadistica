0c1424db0cff999813665c811593f8b82f1cb4c2
Camilo <juancmaringcu@gmail.com>
2021-01-23 21:17:48 -0500

    edit

--- binomial.py
+++ binomial.py
@@ -6,6 +6,15 @@ def convinatoria(n, x):
     return factorial(n)/(factorial(x)*factorial(n-x))
 
 def dBinomial(n,x,p):
-    return convinatoria(n,x)*(p**x)*(1-p)**(n-x)
+    return round(convinatoria(n,x)*(p**x)*(1-p)**(n-x),2)
 
-print(dBinomial(18,2,0.1))
+def rango(c, f, p):
+    final = 0
+    for i in range(c, 4):
+        s = dBinomial(f,i,p)
+        final+=s
+        print(s, final)
+    return final
+
+print(dBinomial(18,4,0.1))
+print(round(1-rango(0,18,0.1),2))
